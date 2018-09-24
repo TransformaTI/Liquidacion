52efc9225d67262ff459c2f959e2173f1e023d2f
ASUS-PC\Diego <binniza2011@gmail.com>
2018-09-24 16:10:07 -0500

    MCC-Actulizacion Idcrm en Liquidacion Web

--- Liquidacion/DLLiquidacion/DatosRegistroPago.cs
+++ Liquidacion/DLLiquidacion/DatosRegistroPago.cs
@@ -712,13 +712,14 @@ namespace SigametLiquidacion
         {
             for (int index2 = 0; index2 <= dtPedidos.Rows.Count - 1; ++index2)
             {
-                this._dataAccess.ModifyData("spLIQ2ActualizaDatosCRM", CommandType.StoredProcedure, new SqlParameter[5]
+                this._dataAccess.ModifyData("spLIQ2ActualizaDatosCRM", CommandType.StoredProcedure, new SqlParameter[6]
                                {
                                 new SqlParameter("@Anio", (object) Convert.ToInt32(dtResumenLiquidacion.Rows[0]["AñoAtt"])),
                                 new SqlParameter("@Folio", (object) Convert.ToInt32(dtResumenLiquidacion.Rows[0]["Folio"])),
                                  new SqlParameter("@Cliente", (object) Convert.ToInt32(dtPedidos.Rows[index2]["Cliente"])),
                                 new SqlParameter("@IdCRM", (object) Convert.ToInt32(dtPedidos.Rows[index2]["IdCRM"])),
-                                new SqlParameter("@Consecutivo", (object) Convert.ToInt32(dtPedidos.Rows[index2]["ConsecutivoOrigen"]))
+                                new SqlParameter("@Consecutivo", (object) Convert.ToInt32(dtPedidos.Rows[index2]["ConsecutivoOrigen"])),
+                                new SqlParameter("@Pedido", (object) Convert.ToInt32(dtPedidos.Rows[index2]["Pedido"]))
 
 
                               });
